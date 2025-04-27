154155fa68ed758960310f52307420a3db3bf874
Prajien Kanth <93981463+PrajienKanth@users.noreply.github.com>
2025-04-27 17:18:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,6 @@ def analyze():
     result = gemini.analyze_image(file_path)
     return jsonify({"analysis_results": result}), 200
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
